2f4bac3fe958aa23bf589502293d9a41f7d61985
Derek Nuzum <derek.nuzum@gmail.com>
2016-03-19 12:10:15 -0700

    appears to be getting results, but  throwing errors

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.searchTerm = '';
+  $scope.gifs = [];
+
+  // $scope.$watch('searchTerm', function(newValue, oldValue) {
+  //   $scope.search();
+  // });
+
+  $scope.search = function() {
+    var req = {
+      url: 'http://api.giphy.com/v1/gifs/search',
+      method: 'GET',
+      params: {
+        q: $scope.searchTerm,
+        api_key: 'dc6zaTOxFJmzC'
+      }
+    };
+
+    $http(req).then(function success(res) {
+      $scope.gifs = res.data.data;
+      console.log($scope.gifs);
+    }, function error(res) {
+      console.log(res)
+    });
+  }
+
+}]);
